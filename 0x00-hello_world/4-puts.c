216c8a8291bcd7df63876e8da217d0555b7cd306
Oluwatomisin-code <tomisintomori@gmail.com>
2022-03-16 20:11:04 +0000

    4-puts.c is a simple c program that prints to standard output

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main function
+ * return statement included
+ */
+int main(void)
+{
+	/**
+	 *puts function prints to standard output
+	 */
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
