f7d25c1bba1fd8e87cd50d281e2fa80b9df9357f
Oluwatomisin-code <tomisintomori@gmail.com>
2022-03-16 23:43:43 +0000

    1-last_digit using if else in c code

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,6 +11,7 @@
 int main(void)
 {
 	int n;
+	unsigned int lastDigit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
@@ -18,12 +19,12 @@ int main(void)
 	lastDigit = n % 10;
 	if (lastDigit > 5)
 	{
-		printf("Last digit of %d is %u and is greater than 5\n", n, lastDigit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 	} else
 	{
 		if (lastDigit == 0)
 		{
-			printf("Last digit of %d is %u and is 0\n", n, lastDigit);
+			printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 		} else
 		{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
