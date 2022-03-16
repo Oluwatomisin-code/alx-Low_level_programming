8b1a31043ac55770b32207d7b333e1eae56ef654
Oluwatomisin-code <tomisintomori@gmail.com>
2022-03-16 23:41:20 +0000

    1-last_digit using if else in c code

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,17 +15,18 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 5)
+	lastDigit = n % 10;
+	if (lastDigit > 5)
 	{
-		printf("Last digit of n is %u and is greater than 5\n", n);
+		printf("Last digit of %d is %u and is greater than 5\n", n, lastDigit);
 	} else
 	{
-		if (n == 0)
+		if (lastDigit == 0)
 		{
-			printf("Last digit of n is %i and is 0\n", n);
+			printf("Last digit of %d is %u and is 0\n", n, lastDigit);
 		} else
 		{
-		printf("Last digit of n is %d and is less than 6 and not 0\n", n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
 		}
 	}
 	return (0);
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * diplays alphabet a -z
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		printf("%c", a);
+}
