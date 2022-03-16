ad544c260832d20f1a2c32f20f5587ddef9258bc
Oluwatomisin-code <tomisintomori@gmail.com>
2022-03-16 21:25:04 +0000

    6-size.c prints the sizes of various types in c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,13 +9,13 @@ int main(void)
 	char c;
 	int i;
 	long int lint;
-	long long llint;
+	long long lli;
 	float f;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(lint));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(llint));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
