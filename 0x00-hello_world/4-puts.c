282251dfc4597669d242a902ce94760ed6140339
Oluwatomisin-code <tomisintomori@gmail.com>
2022-03-16 20:25:45 +0000

    4-puts modified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-/**
- * main function
- * return statement included
+/*
+ * main - Entry point
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	/**
+	/*
 	 *puts function prints to standard output
 	 */
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
