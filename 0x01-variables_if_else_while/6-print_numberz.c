8999a3dbf209802cf07b9a3f6c15228bf56113a7
Oluwatomisin-code <tomisintomori@gmail.com>
2022-03-17 00:46:25 +0000

    6-print_numberz.c was pushed

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   * diplays alphabet a -z
+ *    * Return: 0
+ *     */
+int main(void)
+{
+	int a;
+	
+	for (a = 0; a < 10; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
