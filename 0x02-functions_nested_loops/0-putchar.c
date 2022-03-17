9d908c218dd709b58450129ede7c885a9c27ab88
Oluwatomisin-code <tomisintomori@gmail.com>
2022-03-17 19:24:27 +0000

    Starting 0x02-functions_nested_loops alx task in c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * this program prints _putchar to the console
+ * Return: Return value of 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+README file describes what each file does
