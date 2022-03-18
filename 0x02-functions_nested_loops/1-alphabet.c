01f41ceec882f9936474f6c16f346cba96c39dc5
Oluwatomisin-code <tomisintomori@gmail.com>
2022-03-18 00:53:27 +0000

    learning to create and use header files

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - Entry point
+ * _putchar function include in main.h writes to std
+ * Return: 0 
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{		
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
