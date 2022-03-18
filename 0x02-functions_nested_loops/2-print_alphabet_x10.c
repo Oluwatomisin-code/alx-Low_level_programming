a596f3905439f3a4fe6e5526ee599b7be2d053ae
Oluwatomisin-code <tomisintomori@gmail.com>
2022-03-18 01:29:09 +0000

    2-print_alphabet_x10.c contains a print_alphabet_x10() function that uses a _putchar function contained in _putchar.c file and all this interaction is made possible by the calling the prototype of each function files in a header file called main.h

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabbets 10 times
+ * uses _putchar function found in _putchar.c
+ * check main.h file for _putchar prototype been called
+ * Return: returns void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	while (i <= 10)
+	{
+		for (char a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+	}
+	_putchar('\n')
+}
