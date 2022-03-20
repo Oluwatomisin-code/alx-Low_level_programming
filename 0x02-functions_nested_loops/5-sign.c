5857fa22eecbd622a7c4d22ecd79f16566fbcf72
Oluwatomisin-code <tomisintomori@gmail.com>
2022-03-20 22:37:11 +0000

    5-sign.c prints sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: n is a parameter signifying the received num
+ * Return: returns 0,1 or -1
+ */
+int print_sign(int n)
+{
+	if (n > 1)
+	{
+		return (1);
+	}
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (0);
+}
