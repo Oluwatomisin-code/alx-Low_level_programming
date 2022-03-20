987a907067fe88fb781f04639f089e3ceb5dd87d
Oluwatomisin-code <tomisintomori@gmail.com>
2022-03-20 22:52:36 +0000

    6-abs.c returns absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - returns absolute value of n
+ * @n: parameter to _abs func
+ * Return: returns the abs value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-1(n));
+	return (n);
+}
