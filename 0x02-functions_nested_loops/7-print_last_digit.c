d52883de87f0dab370a147bc6cf626649aedd074
Oluwatomisin-code <tomisintomori@gmail.com>
2022-03-20 23:46:20 +0000

    7-print_last_digit.c returns value of last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,6 +11,13 @@ int print_last_digit(int n)
 	int last_digit;
 
 	last_digit = n % 10;
-	printf('%u', last_digit);
+
+	if (n > 0)
+	{
+		_putchar(last_digit + '0');
+		return (last_digit);
+	}
+	last_digit = (-1 * last_digit);
+	_putchar(last_digit + '0');
 	return (last_digit);
 }
