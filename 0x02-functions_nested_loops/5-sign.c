cb8f7996a8e853fe533870913e3013f2cf6af7c8
Oluwatomisin-code <tomisintomori@gmail.com>
2022-03-20 22:47:07 +0000

    5-sign.c prints sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,12 +9,15 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (+1);
+		_putchar('+');
+		return (1);
 	}
 
 	if (n < 0)
 	{
-		return (-1);
+		_putchar('-');
+		return (1);
 	}
+	_putchar('0');
 	return (0);
 }
