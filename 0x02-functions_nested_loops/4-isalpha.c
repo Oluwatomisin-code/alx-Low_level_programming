179ead1b0c4dbbdea62d5ca7e240c3da50965b0a
Oluwatomisin-code <tomisintomori@gmail.com>
2022-03-20 22:12:06 +0000

    isalpha commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,16 @@
 #include "main.h"
 #include <ctype.h>
 
+/**
+ * _isalpha - check if parameter is an alphabet
+ * @c: function parameter
+ * Return; return 1 or 0
+ */
 int _isalpha(int c)
 {
-	return(isalpha(c));
+	if ( c >= 'a' || c >= 'A' && c <= 'z' || c <= 'Z')
+	{
+		return (1);
+	}
+	return(0);
 }
