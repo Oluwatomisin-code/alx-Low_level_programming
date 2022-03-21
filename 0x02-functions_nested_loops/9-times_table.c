88d6e4537d16945e3afb360a7c628a8f06923c3b
Oluwatomisin-code <tomisintomori@gmail.com>
2022-03-21 23:19:07 +0000

    9-times_table.c prints the times table up to 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * times_table - prints times table from 0 to 9
+ * Return: returns void
+ */
+void times_table(void)
+{
+	int i;
+
+	for (i = 0; i < 12; i++)
+	{
+		int j;
+
+		for (j = 0; j < 9; j++)
+		{
+			printf("%d\, "(i * j));
+
+		}
+		printf("\n")
+	}
+}
