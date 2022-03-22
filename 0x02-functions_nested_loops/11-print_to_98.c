99005e1fa5ac45bd843dc363e9229e4a9f311267
Oluwatomisin-code <tomisintomori@gmail.com>
2022-03-22 00:14:07 +0000

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main"
+/**
+ * print_to_98 - print from n to 98
+ * @n: int parameter passed
+ * Return: returns void
+ */
+void print_to_98(int n)
+{
+	_putchar(n + '0');
+	while (n <= 98)
+	{
+		n = n++;
+		_putchar(n + '0');
+	}
+}
