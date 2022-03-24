a9ec235f7be242b51dc9f5648349ad755e09158a
Oluwatomisin-code <tomisintomori@gmail.com>
2022-03-24 18:10:27 +0000

    debugging process tasks done

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * * main - tests function prints if integer positive or negative
- * * Return: 0
- * */
+ * main - tests function prints if integer positive or negative
+ * Return: 0
+ **/
 
 int main(void)
 {
