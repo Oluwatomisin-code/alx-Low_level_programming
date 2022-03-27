11a010abb4e0e04c71b79243b1386332c28270b7
Oluwatomisin-code <tomisintomori@gmail.com>
2022-03-27 22:09:56 +0000

    task 0 of pointers arrays and strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 
 /**
  * reset_to_98 - this functions resets the value of the param to 98
- * @*n: the parameter that is to be reset to 98
+ * @n: the parameter that is to be reset to 98
  * Return: void
  */
 void reset_to_98(int *n)
