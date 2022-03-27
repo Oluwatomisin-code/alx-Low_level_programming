55149dd3593f03be9aea365a20104ec7a754c205
Oluwatomisin-code <tomisintomori@gmail.com>
2022-03-27 22:04:25 +0000

    task 0 of pointers arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - this functions resets the value of the param to 98
+ * @*n: the parameter that is to be reset to 98
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+	print("%x\n", *n);
+}
