bebd5e02c9209f99931fedf9a7697ddef1e0d826
Oluwatomisin-code <tomisintomori@gmail.com>
2022-03-27 23:12:28 +0000

    4-print_rev.c prints a string pointer in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,7 +7,7 @@
  */
 void print_rev(char *s)
 {
- 	int length = 0;
+	int length = 0;
 
 	while (s[length] != '\0')
 	{
@@ -18,6 +18,7 @@ void print_rev(char *s)
 	{
 		printf("%c", s[length - 1]);
 		length--;
+		if (length = 0)
+			printf("\n");
 	}
-	printf("\n");
 }
