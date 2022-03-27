3d6b20fa56467ff0fb7feb5f55d4daafddd2b52f
Oluwatomisin-code <tomisintomori@gmail.com>
2022-03-27 22:43:00 +0000

    3-puts.c prints a pointer string to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string passed to stdout
+ * @str: string parameter
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] = '\0')
+	{
+		printf("%c", *str[i]);
+		i++;
+	}
+	printf("\n");
+}
