74aafab39f84dc50d6c19a34de2c7afaac0eed8c
Oluwatomisin-code <tomisintomori@gmail.com>
2022-03-28 00:37:28 +0000

    8-print_array.c prints a pointed array n number of times

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - iterates n times through array a and prints them
+ * @a: array pointed to
+ * @n number of items to iterate through
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	while (i != (n-1))
+	{
+		if (i == (n-1))
+		{
+			printf("%u\n", a[i]);
+		}
+		else
+		{
+			printf("%u, ", a[i]);
+		}
+		i++;
+	}
+}
