b7b984c711c41822d49203b01c1ca1067d083504
Oluwatomisin-code <tomisintomori@gmail.com>
2022-03-28 00:44:09 +0000

    8-print_array.c prints a pointed array n number of times

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,11 +14,11 @@ void print_array(int *a, int n)
 	{
 		if (i == (n - 1))
 		{
-			printf("%u\n", a[i]);
+			printf("%d\n", a[i]);
 		}
 		else
 		{
-			printf("%u, ", a[i]);
+			printf("%d, ", a[i]);
 		}
 		i++;
 	}
